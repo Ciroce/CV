40dda652dbd862f3a6bbd6085605feb706ac2481
Ciroce <mf.baquera@gmail.com>
2022-03-23 20:49:03 -0300

    Update gral.js
    gral.,js

--- javascript/gral.js
+++ javascript/gral.js
@@ -1,11 +1,17 @@
 let resultado = sumar(1,2);
 const resultadoResta = resta(1,2);
-console.log(resultado)
-console.log(resultadoResta);
+mostrarEnConsola(resultado)
+mostrarEnConsola(resultadoResta)
+function mostrarEnConsola(valor){
+    console.log(valor);
+}
 function sumar(num1,num2){
     return num1 + num2;
 }
 function resta(num1,num2){
     let resultado = num1 - num2;
     return resultado;
+}
+function mostrarEnConsola(valor){
+    console.log(valor);
 }

d9c3113c5141237440dc7f9f30b46aded8b1b77c
Ciroce <mf.baquera@gmail.com>
2022-03-23 20:32:35 -0300

    sumas restas
    ea

---
+++ javascript/gral.js
@@ -0,0 +1,11 @@
+let resultado = sumar(1,2);
+const resultadoResta = resta(1,2);
+console.log(resultado)
+console.log(resultadoResta);
+function sumar(num1,num2){
+    return num1 + num2;
+}
+function resta(num1,num2){
+    let resultado = num1 - num2;
+    return resultado;
+}
